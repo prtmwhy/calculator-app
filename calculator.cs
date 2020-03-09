5606444cadd404b3659b3cd1803432c0363f3296
prtmwhy <yogawahyupratama123@gmail.com>
2020-03-09 16:53:35 +0700

    Menambahkan fungsi Penambahan

---
+++ calculator.cs
@@ -0,0 +1,16 @@
+class Calculator
+{
+	static void Main(String[] args)
+	{
+		
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
